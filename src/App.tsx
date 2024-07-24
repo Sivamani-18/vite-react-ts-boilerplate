d9cd6b5ce7d809ca2871f114719c6a51b28ff5e3
Sivamani-18 <siva@mindwaveventures.com>
2024-07-24 17:26:29 +0530

    added basename

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Header from './components/Header/Header';
 
 function App() {
   return (
-    <Router>
+    <Router basename='/vite-react-ts-boilerplate'>
       <div className='min-h-screen bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark'>
         <div className='mb-10'>
           <Header />
