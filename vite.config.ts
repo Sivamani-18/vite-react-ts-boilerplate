af5db2199d0c97fe886e407730293267f7471cc9
Sivamani-18 <siva@mindwaveventures.com>
2024-07-24 18:41:09 +0530

    Update Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base:
-    process.env.NODE_ENV === 'production'
-      ? process.env.REACT_APP_BASENAME
-      : '/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  if (mode === 'production') {
+    process.env.NODE_ENV = 'production';
+  }
+
+  return {
+    plugins: [react()],
+    base: process.env.REACT_APP_BASENAME || '/',
+  };
 });
