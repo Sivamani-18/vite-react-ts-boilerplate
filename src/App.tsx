4b6611c628f6ff6ae842436abdd3758915ecf93d
Sivamani-18 <siva@mindwaveventures.com>
2024-07-24 17:46:52 +0530

    update basename vite-react-ts-boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,12 @@ import Header from './components/Header/Header';
 
 function App() {
   return (
-    <Router basename='/'>
+    <Router basename='/vite-react-ts-boilerplate/'>
       <div className='min-h-screen bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark'>
         <div className='mb-10'>
           <Header />
         </div>
+
         <div className='container mx-auto p-6'>
           <Routes>
             <Route path='/' element={<Home />} />
